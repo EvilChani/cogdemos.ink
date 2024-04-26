0f5c28409ec43d9ebb4aa3acd871fb55da6cab80
EvilChani <evilchani@fastmail.fm>
2024-04-26 22:44:20 +0000

    added code and setup for password reset

---
+++ hid/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+// Configuration of Mail Server
+
+return [
+    'smtp_host' => 'sandbox.smtp.mailtrap.io', // STMP Host Server
+    'smtp_port' => '2525', // STMP Port
+    'smtp_username' => 'xxxx', // STMP Username
+    'smtp_password' => 'xxxx' // STMP Password
+];
+
